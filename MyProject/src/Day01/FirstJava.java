ab452214a574fa6f81274c54f8581a0d3ff7c1a0
npnghsw1216 <npnghsw1216@naver.com>
2022-06-19 22:27:43 +0900

    Day01 FirstJava

--- MyProject/src/Day01/FirstJava.java
+++ MyProject/src/Day01/FirstJava.java
@@ -4,7 +4,10 @@ public class FirstJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/**
+		 *   main program
+		 */
+		System.out.println("First Program");	
 	}
 
 }

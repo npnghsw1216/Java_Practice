b8b5aab9d6fdc06a86c3428ed532410251bab634
npnghsw1216 <npnghsw1216@naver.com>
2022-06-26 22:25:48 +0900

    Day06 Inher_2_Parent Class생성

---
+++ MyProject/src/Day06/Inher_2/Parent.java
@@ -0,0 +1,14 @@
+package Day06.Inher_2;
+
+public class Parent {
+	int number = 3;
+	
+	Parent() {
+		System.out.println("Create Parent Obejct");
+	}
+	
+	void eat() {
+		System.out.println("소고기 2인분을 먹었습니다.");
+	}
+
+}

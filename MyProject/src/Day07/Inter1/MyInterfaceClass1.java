905c075c354a06d2800a2af09e26382411aed6ef
npnghsw1216 <npnghsw1216@naver.com>
2022-07-02 19:09:22 +0900

    Day07 MyInterfaceClass1

---
+++ MyProject/src/Day07/Inter1/MyInterfaceClass1.java
@@ -0,0 +1,9 @@
+package Day07.Inter1;
+
+public class MyInterfaceClass1 implements Myinterface1 {
+	public int aaa = 10;
+	
+	public void runMethod1() {
+		
+	}
+}
--- MyProject/src/Day07/Inter1/Myinterface1.java
+++ MyProject/src/Day07/Inter1/Myinterface1.java
@@ -1,6 +1,6 @@
 package Day07.Inter1;
 
-public interface MyInterface1 {
+public interface Myinterface1 {
 	int aaa = 500;
 	void runMethod1();
 }

ddec725f0897cdd5ce634e355fe879c140b9b49b
npnghsw1216 <npnghsw1216@naver.com>
2022-07-02 19:22:33 +0900

    Day07 Class1

---
+++ MyProject/src/Day07/Class1.java
@@ -0,0 +1,15 @@
+package Day07;
+
+public class Class1 {
+	Class1(){
+		System.out.println("Class1 Created");
+		Class2 c = new Class2();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class1 c = new Class1();
+	}
+}
+
+

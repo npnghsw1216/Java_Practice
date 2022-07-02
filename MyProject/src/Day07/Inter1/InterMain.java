6d9ae6826647d068b880903a353ad498c80a29c2
npnghsw1216 <npnghsw1216@naver.com>
2022-07-02 19:10:15 +0900

    Day07 InterMain

---
+++ MyProject/src/Day07/Inter1/InterMain.java
@@ -0,0 +1,11 @@
+package Day07.Inter1;
+
+public class InterMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyInterfaceClass1 mc = new MyInterfaceClass1();
+		System.out.println(mc.aaa);
+	}
+
+}

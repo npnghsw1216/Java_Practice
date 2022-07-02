a27c8100d2c5a89644e881e805e6b84e61a360b5
npnghsw1216 <npnghsw1216@naver.com>
2022-07-02 19:17:18 +0900

    Day07 Parent

---
+++ MyProject/src/Day07/Inter3/Parent.java
@@ -0,0 +1,11 @@
+package Day07.Inter3;
+
+public class Parent {
+	public void func() {
+		System.out.println("Parent의 f");
+	}
+	
+	public static void func2() {
+		System.out.println("Parent의 f2");
+	}
+}
